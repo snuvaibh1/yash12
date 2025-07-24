107c8292462f186f830a6b5367ba397051ea1712
snuvaibh1 <flowgenixx@gmail.com>
2025-07-24 13:13:07 +0530

    Updated GlobalResultsMap.tsx

--- src/components/GlobalResultsMap.tsx
+++ src/components/GlobalResultsMap.tsx
@@ -114,7 +114,7 @@ const GlobalResultsMap: React.FC = () => {
           >
             <div className="absolute inset-0">
               <img
-                src="https://i.imgur.com/BZ5LYjN.png"
+                src="https://i.imgur.com/B1TA9Fz.png"
                 alt="World Map"
                 className="w-full h-full object-contain rounded-2xl"
               />
