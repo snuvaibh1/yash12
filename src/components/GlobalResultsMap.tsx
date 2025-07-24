1a3bb918a5e26fe74ca5998a9088d2ab913b4b12
snuvaibh1 <flowgenixx@gmail.com>
2025-07-24 14:28:09 +0530

    Updated GlobalResultsMap.tsx

--- src/components/GlobalResultsMap.tsx
+++ src/components/GlobalResultsMap.tsx
@@ -32,7 +32,7 @@ const GlobalResultsMap: React.FC = () => {
     { flag: '🇫🇷', countryCode: ' ', city: 'France', name: 'Shridhar', result: '-18 lbs', timeframe: '7 weeks', summary: 'Now pain-free and stronger than ever.' },
     { flag: '🏴󠁧󠁢󠁳󠁣󠁴󠁿', countryCode: ' ', city: 'Scotland', name: 'Pravin', result: '-25 lbs', timeframe: '10 weeks', summary: 'Feels more confident inside and out.' },
     { flag: '🇨🇦', countryCode: ' ', city: 'Canada', name: 'Toushaar', result: '-20 lbs', timeframe: '9 weeks', summary: '102kg → 94.5kg with steady progress.' },
-        { flag: '🇦🇺', countryCode: ' ', city: 'Australia', name: 'Prakhar', result: '-20 lbs', timeframe: '9 weeks', summary: '102kg → 94.5kg with steady progress.' },
+        { flag: '🇦🇺', countryCode: ' ', city: 'Australia', name: 'Prakhar', result: '-20 lbs', timeframe: '9 weeks', summary: 'Balanced job & lost 5kg abroad.' },
     
   ];
 
