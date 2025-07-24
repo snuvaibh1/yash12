f5f3bd1e786bbb81a3a8b9e4e24828f8c96bb6d5
snuvaibh1 <flowgenixx@gmail.com>
2025-07-24 17:21:29 +0530

    Updated CinematicHero.tsx

--- src/components/CinematicHero.tsx
+++ src/components/CinematicHero.tsx
@@ -46,7 +46,7 @@ const CinematicHero: React.FC = () => {
       {/* Static Background Image */}
       <div className="absolute inset-0 z-0">
         <img
-          src="https://i.imgur.com/xnNusu7.png"
+          src="https://i.imgur.com/JNHqgCV.png"
           alt="Cinematic Background"
           className="w-full h-full object-cover"
         />
