b214ebe7fedefd41cc785884cb653ebf6c420ead
snuvaibh1 <flowgenixx@gmail.com>
2025-07-24 12:59:08 +0530

    Updated ShredChallengePage.tsx

--- src/pages/ShredChallengePage.tsx
+++ src/pages/ShredChallengePage.tsx
@@ -7,8 +7,7 @@ const ShredChallengePage: React.FC = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
   const [openFAQ, setOpenFAQ] = useState<number | null>(null);
 
-  const roadmapImages = [
-    'https://streamable.com/6kq255',
+
   ];
 
   const features = [
