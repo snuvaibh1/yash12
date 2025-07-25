82715660dcd70089be80c67216c57aaf8a3bb14c
snuvaibh1 <flowgenixx@gmail.com>
2025-07-25 12:43:04 +0530

    Updated RecipeEbookPage.tsx

--- src/pages/RecipeEbookPage.tsx
+++ src/pages/RecipeEbookPage.tsx
@@ -232,7 +232,7 @@ const RecipeEbookPage: React.FC = () => {
               to="/"
               className="inline-flex items-center space-x-3 bg-charcoal-light text-white px-8 py-4 rounded-full font-semibold text-lg hover:bg-charcoal transition-all duration-300"
             >
-              <ArrowLeft className="w-5 h-5" />
+              
               <span> </span>
             </Link>
           </motion.div>
