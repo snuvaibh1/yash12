ef49e107002c9203ec918dc99dd503cbce8f2f61
snuvaibh1 <flowgenixx@gmail.com>
2025-07-26 07:04:09 +0530

    Updated CinematicHero.tsx

--- src/components/CinematicHero.tsx
+++ src/components/CinematicHero.tsx
@@ -81,7 +81,7 @@ const CinematicHero: React.FC = () => {
               transition={{ duration: 0.6, ease: [0.25, 0.46, 0.45, 0.94] }}
             >
               <motion.img
-                src="https://i.imgur.com/RXWtz5S.png"
+                src=" "
                 alt="Champions Lifestyle Logo"
                 className="w-full h-full object-contain filter drop-shadow-2xl"
                 onLoad={() => setLogoLoaded(true)}
