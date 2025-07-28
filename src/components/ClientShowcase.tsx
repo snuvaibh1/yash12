07e3162698c5ba88af7ed76ad3187b1aeec98f8e
snuvaibh1 <flowgenixx@gmail.com>
2025-07-28 16:19:59 +0530

    Updated ClientShowcase.tsx

--- src/components/ClientShowcase.tsx
+++ src/components/ClientShowcase.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 const TransformationsScroll: React.FC = () => {
   const slides = [
-    { url: 'https://i.imgur.com/syOglcV.png', label: ' ' },
-    { url: 'https://i.imgur.com/ipV2FoJ.png', label: ' ' },
-    { url: 'https://i.imgur.com/pfOKWSS.png', label: ' ' },
-    { url: 'https://i.imgur.com/tkrzjKd.png', label: ' ' },
-    { url: 'https://i.imgur.com/5qNDU1a.png ', label: ' ' },
-    { url: 'https://i.imgur.com/dACYwmz.png', label: ' '' },
+    { url: 'https://i.imgur.com/syOglcV.png', label: 'Week 1' },
+    { url: 'https://i.imgur.com/ipV2FoJ.png', label: 'Week 4' },
+    { url: 'https://i.imgur.com/pfOKWSS.png', label: 'Week 8' },
+    { url: 'https://i.imgur.com/tkrzjKd.png', label: 'Week 10' },
+    { url: 'https://i.imgur.com/5qNDU1a.png ', label: 'Week 12' },
+    { url: 'https://i.imgur.com/dACYwmz.png', label: 'Week 16' },
   ];
 
   return (
