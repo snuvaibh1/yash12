1fbe79632e297031567983f7edfe7a76efed4960
snuvaibh1 <flowgenixx@gmail.com>
2025-07-28 16:47:50 +0530

    Updated ClientShowcase.tsx

--- src/components/ClientShowcase.tsx
+++ src/components/ClientShowcase.tsx
@@ -66,7 +66,7 @@ const TransformationsScroll: React.FC = () => {
 <div className="flex justify-center">
   <div className="bg-[#1c1c22] shadow-lg w-[100%] sm:w-[95%] md:w-[90%] lg:w-[80%] xl:w-[70%] rounded-xl">
     <img
-      src="https://i.imgur.com/PG8o7AP.jpeg"
+      src="https://i.imgur.com/bfvQpfq.png"
       alt="Testimonial Collage"
       className="w-full h-auto object-cover rounded-lg"
     />
