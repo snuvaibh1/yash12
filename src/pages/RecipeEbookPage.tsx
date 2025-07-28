e1a7ff318572d94d6df1cd49adf621291cb1896e
snuvaibh1 <flowgenixx@gmail.com>
2025-07-28 15:28:18 +0530

    Updated RecipeEbookPage.tsx

--- src/pages/RecipeEbookPage.tsx
+++ src/pages/RecipeEbookPage.tsx
@@ -50,7 +50,7 @@ const RecipeEbookPage: React.FC = () => {
                 whileTap={{ scale: 0.95 }}
                 onClick={() => window.open('https://rzp.io/rzp/9yScwCz', '_blank')}
               >
-                <span>PRE-ORDER - ₹99</span>
+                <span>ORDER NOW - ₹349</span>
                 <ArrowRight className="w-6 h-6" />
               </motion.button>
             </motion.div>
